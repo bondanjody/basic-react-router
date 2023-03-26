83fe0bbc9ae18b4bab2175f05e46c7546daa03cc
bondanjody <bondanjodys@gmail.com>
2023-03-26 17:41:37 +0700

    Menavigasi (perpindahan antar pages) dengan menggunakan 'Link' bukan 'anchor' (<a>)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
+import { Fragment } from "react";
+import { Link } from 'react-router-dom'
+
 const Home = () => {
-    return <h1>My Home Page.</h1>
+    return <Fragment>
+        <h1>My Home Page.</h1>
+        <p><Link to="/products">Products</Link></p>
+    </Fragment>
+    
 }
 
 export default Home;
