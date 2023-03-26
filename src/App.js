fe5bd3f0fab676e094bf8cc2d2b152d94b5e4831
bondanjody <bondanjodys@gmail.com>
2023-03-26 17:33:18 +0700

    Menambahkan cara lain (old version) dari penggunaan Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+
+// Yang dikomentari adalah cara lain (old version) dalam menggunakan Router
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
   { path: '/', element: <Home />},
