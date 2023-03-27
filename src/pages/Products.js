f056356deeb9102e4e4f0ee7257c37c8125d371e
bondanjody <bondanjodys@gmail.com>
2023-03-27 07:42:21 +0700

    Menambahkan Link pada Dynamic Route

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,22 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
+    const PRODUCTS = [
+        {id: 'p1', title: 'Product 1'},
+        {id: 'p2', title: 'Product 2'},
+        {id: 'p3', title: 'Product 3'}
+    ]
+
     return <Fragment>
         <h1>Products Page.</h1>
         <ul>
-            <li>Products 1</li>
-            <li>Products 2</li>
-            <li>Products 3</li>
+            {/* <li><Link to='/products/product1'>Products 1</Link></li>
+            <li><Link to='/products/product2'>Products 2</Link></li>
+            <li><Link to='/products/product3'>Products 3</Link></li> */}
+            {PRODUCTS.map(productData => <li key={productData.id}>
+                <Link to={`/products/${productData.id}`}>{productData.title}</Link>
+            </li>)}
         </ul>
     </Fragment>
 }
