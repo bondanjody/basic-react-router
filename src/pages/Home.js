dd76d2464342cb0777d29f8cccbc6b3530577ae8
bondanjody <bondanjodys@gmail.com>
2023-03-27 07:11:37 +0700

    Melakukan Navigasi secara programatik. Misalnya di sini kita memiliki button yang berfungsi mengarahkan ke halaman products. Cara ini sebaiknya jarang digunakan, karena sebaiknya kita menggunakan link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import { Fragment } from "react";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const navigateHandler = () => {
+        navigate('/products');
+    }
+
     return <Fragment>
         <h1>My Home Page.</h1>
         <p><Link to="/products">Products</Link></p>
+        <button onClick={navigateHandler}>Go to Products</button>
     </Fragment>
     
 }
